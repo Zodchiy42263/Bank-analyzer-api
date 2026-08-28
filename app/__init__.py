c8e36f6c90157db606e52e7ea66f83c30846e2c3
Vladislav_2002 <vladik198014@gmail.com>
2026-08-28 18:07:57 +0400

    add requests and change the way the secret key is determined

--- app/__init__.py
+++ app/__init__.py
@@ -25,9 +25,9 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "your-secret-key"
     app.json.sort_keys = False
     
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev-insecure-change-me")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
         "DATABASE_URL",
         "postgresql+psycopg://postgres:postgres@localhost:5432/bank",
