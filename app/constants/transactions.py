7291ebe94272c9310a1070f030208398b8f5a758
Vladislav_2002 <vladik198014@gmail.com>
2026-09-05 12:47:18 +0400

    refactor: move transaction types to constants

---
+++ app/constants/transactions.py
@@ -0,0 +1,17 @@
+# Типы транзакций
+TRANSACTION_TYPES = {
+    "income": [
+        "salary",
+        "deposit_interest",
+        "savings_account_interest",
+        "coupons",
+        "dividends",
+        "incoming_transfers",
+        "loan",
+        "mortgage"
+    ],
+    "expense": [
+        "outgoing_transfers",
+        "expenses",
+    ],
+}
